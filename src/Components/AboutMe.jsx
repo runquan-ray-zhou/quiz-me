e297536e91fc00ef2b5e7f4611df444a52f9a32a
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-08-29 01:29:07 -0400

    add iframe

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -2,6 +2,14 @@ export default function AboutMe() {
     return (
         <div>
             <p>Quiz-Me by: Runquan (Ray) Zhou</p>
+            <iframe
+                width="300"
+                height="175"
+                style={{border: "none"}}
+                src="https://test-ar-marker-react.netlify.app/"
+                allowFullScreen
+                allow="camera"
+            ></iframe>
         </div>
     );
 }
